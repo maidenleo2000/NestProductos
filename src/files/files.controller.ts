8b7ba215ab6921968cb0951b6e99e2b971afd8e8
Leandro Martello <maidenleo2000@gmail.com>
2024-08-25 22:06:40 -0300

    Finalizada clase 162 servir archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,27 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { Response } from 'express';
+
 import { fileFilter } from './helpers/fileFilter.helper';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res:Response, //con este decorador Nest entiende que yo estoy mandando una respuesta y ya no la manda de forma automatica
+    @Param('imageName') imageName: string
+  ){
+
+    const path = this.filesService.getStaticProductImage(imageName)
+    // return path;
+
+    // res.status(403).json({
+    //   ok: false,
+    //   path: path,
+    // })
+    
+    res.sendFile(path); //si la imagen existe me la devuelve pero sin mostrar el path y si no existe me da un error
+  }
+
   @Post('product')
-  @UseInterceptors( FileInterceptor('file', {fileFilter: fileFilter}),  )
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter,
+    // limits: { fieldSize: 1000 },
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    })
+  }),  )
   uploadProductImage( 
     @UploadedFile() file: Express.Multer.File) {
 
     // console.log(file);
-    console.log({fileInController: file});
+    // console.log({fileInController: file});
 
     if (!file) {
       throw new BadRequestException('File not found');
     }
 
-    return {
-      fileName: file.originalname
-    };
+
+    const secureUrl = `${file.filename}`;
+    return { secureUrl };
+
+    // return {
+    //   fileName: file.originalname
+    // };
   }
 }
